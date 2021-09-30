42dc40c3a83aa0a69df63e86b520064fe1301fdf
Boushy97 <Boush@KO-PC>
2021-09-30 18:36:24 +0300

    Initial Author Routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const expressLayouts = require('express-ejs-layouts')
 
 // Importing Router
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 // Set up views and layouts and their paths.
 app.set('view engine', 'ejs')
@@ -16,10 +17,11 @@ app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-
+app.use(express.urlencoded({ limit: '10mb', extended: false}))
 
 // Set up mongoose
 const mongoose = require('mongoose')
+const { urlencoded } = require('express')
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser:true})
 const db = mongoose.connection
 db.on('error', error => console.error(error))
@@ -28,5 +30,7 @@ db.once('open', () => console.log('Connected to Mongoose'))
 
 // Using Routers
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 
 app.listen(process.env.PORT || 3000)
